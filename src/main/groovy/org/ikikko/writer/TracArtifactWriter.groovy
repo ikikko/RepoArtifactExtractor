bddc251915be946d5f7a5ca641aa2b9f8357a3a0
ikikko <ikikko+github@gmail.com>
2011-08-28 22:00:03 +0900

    XML-RPCライブラリに、Groovy 1.8用に対応を追加する

--- src/main/groovy/org/ikikko/writer/TracArtifactWriter.groovy
+++ src/main/groovy/org/ikikko/writer/TracArtifactWriter.groovy
@@ -2,7 +2,39 @@ package org.ikikko.writer
 
 import groovy.net.xmlrpc.XMLRPCServerProxy
 
+import org.codehaus.groovy.runtime.EncodingGroovyMethods
+
 class TracArtifactWriter implements ArtifactWriter {
+
+	/**
+	 * setBasicAuth()内でDeprecatedなメソッドを使用していてGroovy 1.8では動かないので、修正を加えています。
+	 * 
+	 * @see http://jira.codehaus.org/browse/GMOD-239
+	 */
+	class XMLRPCServerProxy18 extends XMLRPCServerProxy {
+
+		XMLRPCServerProxy18(String paramString) {
+			super(paramString)
+		}
+
+		@Override
+		public void setBasicAuth(String paramString1, String paramString2)
+		throws IOException {
+			if (null == paramString1) {
+				this.authString = null;
+				return;
+			}
+
+			String str = paramString1 + ":" + paramString2;
+			try {
+				// this.authString = "Basic " + DefaultGroovyMethods.encodeBase64(str.getBytes("ISO-8859-1")).toString();
+				this.authString = "Basic " + EncodingGroovyMethods.encodeBase64(str.getBytes("ISO-8859-1")).toString();
+			}
+			catch (UnsupportedEncodingException localUnsupportedEncodingException) {
+				throw new IOException("Error encoding auth header: " + localUnsupportedEncodingException.getMessage());
+			}
+		}
+	}
 
 	static def separator = System.properties['line.separator']
 
@@ -17,7 +49,7 @@ class TracArtifactWriter implements ArtifactWriter {
 
 	@Override
 	def init() {
-		proxy = new XMLRPCServerProxy("${url}rpc")
+		proxy = new XMLRPCServerProxy18("${url}rpc")
 		content << "|| '''groupId''' || '''artifactId''' || '''version''' || '''Ivy Depencency''' ||$separator"
 	}
 
